38bc01e14d189b861db793571f43a465cbdde329
Walker <walkerrm@gmail.com>
2020-10-24 23:50:35 -0400

    remove modules from github

--- index.js
+++ index.js
@@ -3,21 +3,38 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
 
 program
-    .version('1.0.0')
+    .version('0.0.1')
     .argument('[filename]', 'Name of a file to execute')
-    .action((args) => {
-        console.log(args);
+    .action(async ({ filename }) => {
+        const name = filename || 'index.js';
+        
+        try {
+            await fs.promises.access(name);
+        } catch (err) {
+            throw new Error(`Could not find the file ${name}`);
+        }
+
+        let proc;
+        const start = debounce(() => {
+
+            if (proc) {
+                proc.kill();
+            }
+            console.log(chalk.blue('>>>> Starting process...'));
+            proc = spawn('node', [name], { stdio: 'inherit' });
+        }, 100);
+        
+        chokidar
+            .watch('.')
+            .on('add', start)
+            .on('change', start)
+            .on('unlink', start);
+        
     });
 
-const start = debounce(() => {
-    console.log('starting users program');
-}, 100);
-
-chokidar
-    .watch('.')
-    .on('add', start)
-    .on('change', () => console.log('File changed'))
-    .on('unlink', () => console.log('File linked'));
 
